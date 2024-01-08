7b4ec3f51c05c8d4f233a6b5ba3df633294d5be1
orshapiro1 <155963734+orshapiro1@users.noreply.github.com>
2024-01-08 13:07:30 +0200

    sta

--- nfgn.py
+++ nfgn.py
@@ -1,31 +1,30 @@
-
 job = int(input("אם אתה מורה הכנס את המספר 0 אם אתה תלמיד הכנס את המספר 1"))
 if job == 0:
     teacher_name = input("הכנס את השם שלך")
     teacher_email = input("הכנס את כתובת המייל שלך")
     teacher_subject = input("איזה מקצוע אתה מלמד?")
     if teacher_subject == "מתמטיקה":
-        math_teacher=int(input("מה מספר היחידות שאתה מלמד?"))
-    if teacher_subject=="אנגלית":
-        english_teacher=int(input("מה מספר היחידות שאתה מלמד?"))
-    grade10=input("אתה מלמד את כיתה י?")
-    if grade10=="כן":
-        num_grade10=input("מה מספרי הכיתות שאתה מלמד(הפרד בין הכיתות בפסיקים)")
-    grade11=input("אתה מלמד את כיתה יא?")
-    if grade11=="כן":
-        num_grade11=input("מה מספרי הכיתות שאתה מלמד(הפרד בין הכיתות בפסיקים)")
+        math_teacher = int(input("מה מספר היחידות שאתה מלמד?"))
+    if teacher_subject == "אנגלית":
+        english_teacher = int(input("מה מספר היחידות שאתה מלמד?"))
+    grade10 = input("אתה מלמד את כיתה י?")
+    if grade10 == "כן":
+        num_grade10 = input("מה מספרי הכיתות שאתה מלמד(הפרד בין הכיתות בפסיקים)")
+    grade11 = input("אתה מלמד את כיתה יא?")
+    if grade11 == "כן":
+        num_grade11 = input("מה מספרי הכיתות שאתה מלמד(הפרד בין הכיתות בפסיקים)")
     grade12 = input("אתה מלמד את כיתה יב?")
-    if grade12=="כן":
-        num_grade12=input("מה מספרי הכיתות שאתה מלמד(הפרד בין הכיתות בפסיקים)")
+    if grade12 == "כן":
+        num_grade12 = input("מה מספרי הכיתות שאתה מלמד(הפרד בין הכיתות בפסיקים)")
 
-#users = open("users.text", "r")
+# users = open("users.text", "r")
 users = open("users.text", "a")
 users.write(teacher_name + "," + teacher_email + "," + teacher_subject)
 print(users.read())
 
-if job==1:
-    student_name=input("הכנס את השם שלך")
-    student_email=input("הכנס את כתובת המייל שלך")
+if job == 1:
+    student_name = input("הכנס את השם שלך")
+    student_email = input("הכנס את כתובת המייל שלך")
     ca = input("איזה כיתה אתה?")
     h = input("איזה שכבה אתה?")
     student_hours_dictionary = {
@@ -75,8 +74,6 @@ if job==1:
     student_hours_dictionary["tuesday"][8] = input("הכנס שעה 8 יןם שלישי(אם אין שיעור הכנס רווח)")
     student_hours_dictionary["tuesday"][9] = input("הכנס שעה 9 יןם שלישי(אם אין שיעור הכנס רווח)")
     student_hours_dictionary["tuesday"][10] = input("הכנס שעה 10 יןם שלישי(אם אין שיעור הכנס רווח)")
-    
-    
 
 
     def h1():
@@ -84,7 +81,7 @@ if job==1:
             s1 = input("מה המגמה הראשונה שלך")
             s2 = input("מה המגמה השנייה שלך")
             points_math = int(input("כמה יחידות אתה במתמטטיקה"))
-            points_english=int(input("כמה יחידות אתה באנגלית"))
+            points_english = int(input("כמה יחידות אתה באנגלית"))
 
         class ca2:
             s1 = input("מה המגמה הראשונה שלך")
@@ -196,6 +193,7 @@ if job==1:
             points_math = int(input("כמה יחידות אתה במתמטטיקה"))
             points_english = int(input("כמה יחידות אתה באנגלית"))
 
+
     def h3():
         class ca1:
             s1 = input("מה המגמה הראשונה שלך")
@@ -246,12 +244,4 @@ if job==1:
             points_english = int(input("כמה יחידות אתה באנגלית"))
 
 
-
-
-if h = "י":
-    h1()
-elif h = 'יא':
-    h2()
-elif h = 'יב':
-    h3()
 
